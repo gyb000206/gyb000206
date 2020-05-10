1272fc66c5193c6fe58a7440966401e0875e0bf0
gyb000206 <60536348+gyb000206@users.noreply.github.com>
2020-05-10 20:27:05 +0800

    Update test01.java

--- test01.java
+++ test01.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static boolean Int_memo = true;        //判断输入形式
+
     /*机位分配*/
     public static void PC_numbers(int i) {
         System.out.println("你的机位是" + i + "号");
@@ -11,7 +14,7 @@ public class Main {
         int pay_val = 0;
         pay_val = Int - val;
         System.out.println("找您" + pay_val + "元");
-    }
+}
 
     /*支付控制模块*/
     public static void pay_Int(int mon) {
@@ -21,7 +24,6 @@ public class Main {
         if (Int >= mon) {
             System.out.println("开票成功");
             back_money(Int, 6);
-
         } else {
             System.out.println("钱不够");
         }
@@ -35,25 +37,30 @@ public class Main {
         System.out.println("2---包时");
         System.out.println("3---夜机");
         int time = in.nextInt();
-        switch (time) {
-            case 1: {
-//                System.out.println("请支付6元");
-                pay_Int(6);
-                break;
+//        boolean Int_memo = false;
+        while (time > 0 && time < 4) {
+            switch (time) {
+                case 1: {
+                    //                System.out.println("请支付6元");
+                    pay_Int(6);
+                    break;
+                }
+                case 2: {
+                    //                System.out.println("请支付10元");
+                    pay_Int(10);
+                    break;
+                }
+                case 3: {
+                    //                System.out.println("请支付20元");
+                    pay_Int(20);
+                    break;
+                }
+                default:
+                    //                throw new IllegalStateException("Unexpected value: " + time);
             }
-            case 2: {
-//                System.out.println("请支付10元");
-                pay_Int(10);
-                break;
-            }
-            case 3: {
-//                System.out.println("请支付20元");
-                pay_Int(20);
-                break;
-            }
-            default:
-//                throw new IllegalStateException("Unexpected value: " + time);
         }
+        System.out.println("输入错误");
+        Int_memo = false;
     }
 
 
@@ -61,15 +68,18 @@ public class Main {
 //        System.out.println("Hello World");
         Scanner in = new Scanner(System.in);
         int i = 0;
+        int max_Pcnumbers = 2;      //最大容纳量
         System.out.println("欢迎登陆网吧前台管理系统");
         while (true) {
             System.out.println("请输入年龄");
             int age = in.nextInt();
             i++;
             if (age >= 18) {
-                if (i <= 2) {
+                if (i <= max_Pcnumbers) {
                     adult();
-                    PC_numbers(i);
+                    if (Int_memo = true) {
+                        PC_numbers(i);
+                    }
                 } else {
                     System.out.println("机位不够");
                 }
